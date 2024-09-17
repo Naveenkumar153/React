1ee0622ceba567ad4f56c457805c63e06f43be68
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-09-17 06:49:12 +0530

    feat: multi list rendering implemented

--- src/components/project-2/Gameboard.jsx
+++ src/components/project-2/Gameboard.jsx
@@ -1,28 +1,27 @@
+import { useState } from "react";
+
 export default function Gameboard(props) {
+  const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
 
-    const initialGameBoard = [
-        [null, null,null],
-        [null, null,null],
-        [null, null,null],
-    ];
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    return (
-        <ol id="game-board">
-            {
-                initialGameBoard.map((row,rowIdx) => {;
-                    <li key={rowIdx}>
-                        <ol>
-                            {
-                                row.map((playerSymbol,colIdx) => {
-                                    <li key={colIdx}>
-                                       <button> {playerSymbol}</button>
-                                    </li>
-                                })
-                            }
-                        </ol>
-                    </li>
-                })
-            }
-        </ol>
-    );
-};
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIdx) => (
+        <li key={rowIdx}>
+          <ol>
+            {row.map((playerSymbol, colIdx) => (
+              <li key={colIdx}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
