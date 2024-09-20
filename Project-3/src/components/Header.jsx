836962cd6649ffb0af1f480195173d8602dfd215
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-09-20 11:19:14 +0530

    chor: install tailwind css

--- Project-3/src/components/Header.jsx
+++ Project-3/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png';
 import header  from './Header.module.css';
 export default function Header() {
   return (
-    <header>
+    <header className='flex flex-col items-center mt-8 mb-16'>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p className={header.para}>A community of artists and art-lovers.</p>
