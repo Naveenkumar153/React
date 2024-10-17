20d75da918a8de4f64d06e007107bac93110fd87
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-10-17 22:40:28 +0530

    feat: install react & react-dom

--- Nameste-react/script.js
+++ Nameste-react/script.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('h1', { id:'title' }, 
     React.createElement('ul', null, 
         React.createElement('li', null, 'Item 1'),
@@ -5,6 +8,11 @@ const heading = React.createElement('h1', { id:'title' },
         React.createElement('li', null, 'Item 3')
     )
 );
+
+const head2 = <h1 className='heading'>Hello</h1>;
+console.log('heading',heading);
+console.log('head2',head2);
+
 
 // ReactDOM.render(heading, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
